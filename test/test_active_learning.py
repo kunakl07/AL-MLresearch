0a3f48f61f00cd434d25997ddd1079d18d750fca
kunal mehta <36035718+kunakl07@users.noreply.github.com>
2020-08-25 19:13:22 +0530

    Update test_active_learning.py

--- test/test_active_learning.py
+++ test/test_active_learning.py
@@ -7,6 +7,6 @@ class TestPreprocess(unittest.TestCase):
 
     def test_active_learning(self):
         probability_last, class_ten = active_learning.predict("preprocess_mag_scipy_Srkws.h5", "test/calls/")
-        # self.assertEqual(probability_last, 0.52795976)
+
         self.assertEqual(class_ten, 0)
         npt.assert_almost_equal(probability_last, 0.5279, decimal=3)
